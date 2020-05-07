391860c35e7f446be9d1b52c5d852b8b62872a43
Maxwell <passagesfall@gmail.com>
2020-05-07 11:00:40 +0800

    plannercore: ignore invisible indexes in optimizer  (#15421)

--- ddl/table.go
+++ ddl/table.go
@@ -891,7 +891,6 @@ func updateVersionAndTableInfoWithCheck(t *meta.Meta, job *model.Job, tblInfo *m
 		return ver, errors.Trace(err)
 	}
 	return updateVersionAndTableInfo(t, job, tblInfo, shouldUpdateVer)
-
 }
 
 // updateVersionAndTableInfo updates the schema version and the table information.
